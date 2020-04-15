06b20c3849b5ab77c0fdffec785682b437d68b79
Abhi Shah <ashah244@uic.edu>
2020-04-15 17:28:18 -0500

    Cleaned all diseases and GDP datasets

--- DiseaseCleaning.py
+++ DiseaseCleaning.py
@@ -2,44 +2,61 @@ import pandas as pd
 import numpy as np
 import sys
 
+#program thats takes in two paths, one for where the data is and the other one where the path is going to be saved.
 
-def cleaningDataset(directory):
-    df = pd.read_csv(str(directory));
+#example running method: 
+#python3 DiseaseCleaning.py 'datasets/Diseases/Injuries.csv' 'datasets/CleanedDiseases/CleanedInjuries.csv'
+#this will save the Injuries.csv file to the CleanedDiseases path as CleanedInjures.csv
+def cleaningDataset(directoryIn, directoryOutput):
+    #read in the file
+    df = pd.read_csv(str(directoryIn));
+
+    #changes the columns to only look at the ones that have 0-4 years for those specifc years
     for col in df:
         if (col == '2015.2' or col == '2010.2' or  col == '2005.2' or col == '2000.2' or col == 'Unnamed: 0'):
             continue;
         else:
             df = df.drop([str(col)], axis = 1)
     
-    countryList = ['India', 'Philippines', 'Bangladesh', 'China', 'Saudi Arabia', 'Poland', 'Russia', 'Germany', 'Ukraine', 
+    #list of countires that we want to look at 
+    countryList = ['India', 'Philippines', 'Bangladesh', 'China', 'Saudi Arabia', 'Poland', 'Russian Federation', 'Germany', 'Ukraine', 
     'Serbia', 'Albania', 'Ecuador', 'Colombia', 'Brazil', 'Chile', 'Uganda', 'Kenya', 'Ethiopia', 'Morocco', 'South Africa', 'Nigeria', 
-    'Burundi', 'United States', 'Canada', 'Mexico', 'Dominican Republic', 'Guatemala', 'Haiti', 'Australia', 'New Zealand', 'Solomon Islands', 'Fiji']
+    'Burundi', 'United States of America', 'Canada', 'Mexico', 'Dominican Republic', 'Guatemala', 'Haiti', 'Australia', 'New Zealand', 'Solomon Islands', 'Fiji']
     countryList.sort()
-    print(countryList)
+
+    #rename the .2 for 0-4 years of death
     df = df. rename(columns = {'Unnamed: 0': 'Countries', '2015.2': '2015', '2010.2': '2010', '2005.2': '2005', '2000.2': '2000'})
 
+    #removes the Country row that was not relevant
     index = 0
     for country in df['Countries']:
         if (country == 'Country'):
             df = df.drop(index).reset_index(drop=True)
         index = index + 1
 
-    index = 0
+    #drops the two nan rows that were not going to be used
     df = df.dropna().reset_index(drop=True)
-    for country2 in countryList:
-        for country in df['Countries']:
-            if str(country) == str(country2):
-                continue;
-            else:
-                df = df[(df.Countries != country)]
-                
-    print(df.head(10))
 
+    #adjusts the dataframe to only keep the data from the countries list
+    index = 0
+    for country in df['Countries']:
+        if country == countryList[index]:
+            if(index != len(countryList)-1):
+                index = index + 1
+        else:
+            df = df[(df.Countries != country)]
+
+    #reset the index of the rows            
+    df = df.reset_index(drop=True)
+
+    #save the dataframe as a csv to proper directory
+    df.to_csv(directoryOutput, index = False)
+    
         
     
     
 
 
-cleaningDataset(sys.argv[1])
+cleaningDataset(sys.argv[1], sys.argv[2])
 
 
